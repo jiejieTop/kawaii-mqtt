8703dd3bd06d42ca95c01f78f812dcebf004285c
liYony <941843540@qq.com>
2023-08-29 20:38:58 +0800

    优化示例程序，方便用户快速接入

--- test/test.c
+++ test/test.c
@@ -12,6 +12,27 @@
 #include <board.h>
 #include "mqttclient.h"
 
+#ifndef KAWAII_MQTT_HOST
+#define KAWAII_MQTT_HOST               "jiejie01.top"
+#endif
+#ifndef KAWAII_MQTT_PORT
+#define KAWAII_MQTT_PORT               "1883"
+#endif
+#ifndef KAWAII_MQTT_CLIENTID
+#define KAWAII_MQTT_CLIENTID           "rtthread001"
+#endif
+#ifndef KAWAII_MQTT_USERNAME
+#define KAWAII_MQTT_USERNAME           "rt-thread"
+#endif
+#ifndef KAWAII_MQTT_PASSWORD
+#define KAWAII_MQTT_PASSWORD           "rt-thread"
+#endif
+#ifndef KAWAII_MQTT_SUBTOPIC
+#define KAWAII_MQTT_SUBTOPIC           "rtt-sub"
+#endif
+#ifndef KAWAII_MQTT_PUBTOPIC
+#define KAWAII_MQTT_PUBTOPIC           "rtt-pub"
+#endif
 
 static void sub_topic_handle1(void* client, message_data_t* msg)
 {
@@ -30,10 +51,9 @@ static int mqtt_publish_handle1(mqtt_client_t *client)
     msg.qos = QOS0;
     msg.payload = (void *)"this is a kawaii mqtt test ...";
 
-    return mqtt_publish(client, "rtt-pub", &msg);
+    return mqtt_publish(client, KAWAII_MQTT_PUBTOPIC, &msg);
 }
 
-static char cid[64] = { 0 };
 static void kawaii_mqtt_demo(void *parameter)
 {
     mqtt_client_t *client = NULL;
@@ -44,20 +64,18 @@ static void kawaii_mqtt_demo(void *parameter)
 
     client = mqtt_lease();
 
-    rt_snprintf(cid, sizeof(cid), "rtthread%d", rt_tick_get());
-
-    mqtt_set_host(client, "jiejie01.top");
-    mqtt_set_port(client, "1883");
-    mqtt_set_user_name(client, "rt-thread");
-    mqtt_set_password(client, "rt-thread");
-    mqtt_set_client_id(client, cid);
+    mqtt_set_host(client, KAWAII_MQTT_HOST);
+    mqtt_set_port(client, KAWAII_MQTT_PORT);
+    mqtt_set_user_name(client, KAWAII_MQTT_USERNAME);
+    mqtt_set_password(client, KAWAII_MQTT_PASSWORD);
+    mqtt_set_client_id(client, KAWAII_MQTT_CLIENTID);
     mqtt_set_clean_session(client, 1);
 
-    KAWAII_MQTT_LOG_I("The ID of the Kawaii client is: %s ",cid);
+    KAWAII_MQTT_LOG_I("The ID of the Kawaii client is: %s ", KAWAII_MQTT_CLIENTID);
 
     mqtt_connect(client);
     
-    mqtt_subscribe(client, "rtt-sub", QOS0, sub_topic_handle1);
+    mqtt_subscribe(client, KAWAII_MQTT_SUBTOPIC, QOS0, sub_topic_handle1);
     
     while (1) {
         mqtt_publish_handle1(client);
